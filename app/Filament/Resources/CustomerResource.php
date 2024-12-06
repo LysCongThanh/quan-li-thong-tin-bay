9410536adfbffa37598422a071b865b9dd8008f9
ThuiDi <thuyvy2442004@gmail.com>
2024-12-06 21:37:48 +0700

    #5 - ThuiDi create customer resource

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,90 @@
+<?php
+
+    namespace App\Filament\Resources;
+
+    use App\Filament\Resources\CustomerResource\Pages;
+    use App\Filament\Resources\CustomerResource\RelationManagers;
+    use App\Models\Customer;
+    use Filament\Forms;
+    use Filament\Forms\Form;
+    use Filament\Resources\Resource;
+    use Filament\Tables;
+    use Filament\Tables\Table;
+    use Filament\Forms\Components\TextInput;
+    use Filament\Tables\Columns\TextColumn;
+
+    use Illuminate\Database\Eloquent\Builder;
+    use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+    class CustomerResource extends Resource
+    {
+        protected static ?string $model = Customer::class;
+
+
+        protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+        protected static ?string $navigationLabel = 'Khách Hàng';
+
+        protected static ?string $modelLabel = 'khách hàng  ';
+
+        protected static ?string $pluralModelLabel = 'Khách Hàng';
+
+        protected static ?string $navigationGroup = 'Quản lí';
+
+
+        public static function form(Form $form): Form
+        {
+            return $form
+                ->schema([
+                    TextInput::make('name')
+                        ->label('Tên Khách Hàng')
+                        ->required(),
+                    TextInput::make('email')
+                        ->label('Email')
+                        ->email()
+                        ->required(),
+                    TextInput::make('phone')
+                        ->label('Số Điện Thoại'),
+                    TextInput::make('address')
+                        ->label('Địa Chỉ'),
+                ]);
+        }
+
+        public static function table(Table $table): Table
+        {
+            return $table
+                ->columns([
+                    TextColumn::make('name')->label('Tên Khách Hàng')->searchable(),
+                    TextColumn::make('email')->label('Email')->searchable(),
+                    TextColumn::make('phone')->label('Số Điện Thoại'),
+                    TextColumn::make('created_at')->label('Ngày Tạo')->dateTime(),
+                ])
+                ->filters([
+                    //
+                ])
+                ->actions([
+                    Tables\Actions\EditAction::make(),
+                ])
+                ->bulkActions([
+                    Tables\Actions\BulkActionGroup::make([
+                        Tables\Actions\DeleteBulkAction::make(),
+                    ]),
+                ]);
+        }
+
+        public static function getRelations(): array
+        {
+            return [
+                //
+            ];
+        }
+
+        public static function getPages(): array
+        {
+            return [
+                'index' => Pages\ListCustomers::route('/'),
+                'create' => Pages\CreateCustomer::route('/create'),
+                'edit' => Pages\EditCustomer::route('/{record}/edit'),
+            ];
+        }
+    }
