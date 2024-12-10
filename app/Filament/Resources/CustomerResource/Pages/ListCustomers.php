0adf29bd28eb0866947e7f7210a22287e59ad24d
ThuiDi <thuyvy2442004@gmail.com>
2024-12-10 14:35:38 +0700

    # - ThuiDi create Customer

---
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\Pages;
+
+use App\Filament\Resources\CustomerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCustomers extends ListRecords
+{
+    protected static string $resource = CustomerResource::class;
+
+    protected static ?string $breadcrumb = 'Danh Sách';
+
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+
+}
